3d2f1b64ded6ddda8aaf4d768834d11933303d0e
Dima Paschenko <pdv240222@i.ua>
2025-02-27 15:23:50 +0200

    home-6.3

--- lesson-6/all.js
+++ lesson-6/all.js
@@ -1,23 +1,23 @@
 // 1.Створити масив, довжину та елементи якого задає користувач. Після цього, відсортувати масив за зростанням. Далі, видалити з масива елементи з 2-го по 4-й елемент. По мірі змін масива - виводити його вміст на сторінку.
 
 
-let userArr = parseInt(prompt(`enter number array`));
-let arr = [];
+// let userArr = parseInt(prompt(`enter number array`));
+// let arr = [];
 
-for(let i = 0; i < userArr; i++){
-	let elem = prompt(`enter element ${i + 1}`);
-	arr.push(elem);
-}
-document.write(`<p> you enter number: ${arr}</p>`);
+// for(let i = 0; i < userArr; i++){
+// 	let elem = prompt(`enter element ${i + 1}`);
+// 	arr.push(elem);
+// }
+// document.write(`<p> you enter number: ${arr}</p>`);
 
-const result1 = arr.sort(function(a,b){
-	return a - b;
-})
-document.write(`<p> number sorting: ${result1} </p>`);
-const result2 = arr.splice(1, 3);
+// const result1 = arr.sort(function(a,b){
+// 	return a - b;
+// })
+// document.write(`<p> number sorting: ${result1} </p>`);
+// const result2 = arr.splice(1, 3);
 
 
-document.write(`<p> delit element 2-4: ${arr} </p>`);
+// document.write(`<p> delit element 2-4: ${arr} </p>`);
 
 
 // 2. Дано массив [16,-37,54,-4,72,-56,47,4,-16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]
@@ -73,6 +73,34 @@ document.write(`<p> delit element 2-4: ${arr} </p>`);
 // console.log("10. Добуток всіх додатних елементів:", productPositive);
 
 
+
+// 3. Дано масив об’єктів. Вивести масив телефонних номерів користувачів в яких баланс більше ніж 2000 доларів. І знайти суму всіх балансів користувачів
+
+let users = [{index:0,isActive:true,balance:2226.60,name:"Eugenia Sawyer",gender:"female",phone:"+1 (840) 583-3207",address:"949 John Street, Rose, Puerto Rico, 1857"}, {index:1,isActive:true,balance:2613.77,name:"Pauline Gallegos",gender:"female",phone:"+1 (985) 593-3328",address:"328 Greenpoint Avenue, Torboy, North Dakota, 6857"}, {index:2,isActive:false,balance:3976.41,name:"Middleton Chaney",gender:"male",phone:"+1 (995) 591-2478",address:"807 Fleet Walk, Brutus, Arkansas, 9783"}, {index:3,isActive:true,balance:1934.58,name:"Burns Poole",gender:"male",phone:"+1 (885) 559-3422",address:"730 Seba Avenue, Osage, Alabama, 6290"}, {index:4,isActive:true,balance:3261.65,name:"Mcfadden Horne",gender:"male",phone:"+1 (942) 565-3988",address:"120 Scholes Street, Kirk, Michigan, 1018"}, {index:5,isActive:false,balance:1790.56,name:"Suzette Lewis",gender:"female",phone:"+1 (837) 586-3283",address:"314 Dunne Place, Bawcomville, Guam, 9053"}];
+
+let balances = [2226.60, 2613.77, 3976.41, 1934.58, 3261.65, 1790.56];
+let phones = [
+			  "+1 (840) 583-3207", "+1 (985) 593-3328", "+1 (995) 591-2478",
+			  "+1 (885) 559-3422", "+1 (942) 565-3988", "+1 (837) 586-3283"
+];
+		 
+
+let totalBalance = 0;
+let systemOutPrint; // Телефони з балансом > 2000$:
+let sumBalances = 0;
+for (let i = 0; i < balances.length; i++) {
+		totalBalance += balances[i];
+	if (balances[i] > 2000) {
+		systemOutPrint = (phones[i]);
+		console.log(`Телефони з балансом > 2000$: ${systemOutPrint} = ${balances[i]}`);
+		
+		}
+		if(balances[i] > 2000){
+			sumBalances += balances[i];
+			console.log(`Загальна сума балансів: `, sumBalances );
+		}
+		
+};
 
 
 
