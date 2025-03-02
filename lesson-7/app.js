ca629ee74d4f16e07389e46abdc3cb02a1835cdd
Dima Paschenko <pdv240222@i.ua>
2025-03-02 10:51:24 +0200

    home-7.2

--- lesson-7/app.js
+++ lesson-7/app.js
@@ -11,14 +11,31 @@
 // let result = removeChars(inputString, charsToRemove);
 // console.log("Результат:", result);
 
-// 2. Дано масив з елементами різних типів. Створити функцію яка вираховує середнє арифметичне лише числових елементів даного масиву.
+// 2. Дано масив з елементами різних типів, які вводить користувач. Створити функцію яка вираховує середнє арифметичне лише числових елементів даного масиву.
 
 
+function averageOfNumbers(arr) {
+	let sum = 0; // Змінна для збереження суми чисел
+	let count = 0; // Лічильник числових елементів
+	
+	
+	// Перебираємо всі елементи масиву
+	for (let i = 0; i < arr.length; i++) {
+		 if (typeof arr[i] === 'number') { // Перевіряємо, чи елемент - число
+			  sum += arr[i]; // Додаємо число до суми
+			  count++; // Збільшуємо лічильник чисел
+		 }
+	}
+
+	// Якщо чисел немає, повертаємо 0, інакше обчислюємо середнє
+	return count === 0 ? 0 : sum / count;
+}
 
 
-
-
-
+let userInput = prompt(`Введіть eлементи масиву через кому`);
+let userArray = userInput.split(','); 
+let result = averageOfNumbers(userArray); // Викликаємо функцію
+console.log(result); // Виводимо результат в консоль
 
 
 
@@ -29,21 +46,21 @@
 Наприклад:
 
 const array = [1, 3, 4, 6, 2, 5, 7];*/
-const array = [1, 3, 4, 6, 2, 5, 7];
-const itemToRemove = 4;
+// const array = [1, 3, 4, 6, 2, 5, 7];
+// const itemToRemove = 4;
 
 
-function removeElement(array, item) {
-	for(let i = 0; i < array.length; i++){
-		if(i === itemToRemove){
-			array.splice(itemToRemove[i])
-			console.log(array)
-		}
-	}
+// function removeElement(array, item) {
+// 	for(let i = 0; i < array.length; i++){
+// 		if(i === itemToRemove){
+// 			array.splice(itemToRemove[i])
+// 			console.log(array)
+// 		}
+// 	}
 		
-}
-const newArray = removeElement(array, itemToRemove);
-console.log(newArray);  // [1, 3, 6, 2, 5, 7]
+// }
+// const newArray = removeElement(array, itemToRemove);
+// console.log(newArray);  // [1, 3, 6, 2, 5, 7]
 
 
 
